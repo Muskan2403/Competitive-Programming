254f43b48b855d64910223e5069845de3ba07321
Muskan Darji <118981887+Muskan2403@users.noreply.github.com>
2025-07-04 11:27:41 +0530

    Time: 4 ms (47.29%), Space: 41.1 MB (76.23%) - LeetHub

--- 1130-last-stone-weight-ii/1130-last-stone-weight-ii.java
+++ 1130-last-stone-weight-ii/1130-last-stone-weight-ii.java
@@ -6,13 +6,13 @@ class Solution {
             total += num;
         }
 
-        boolean[][] dp = new boolean[n+1][total+1];
+        boolean[][] dp = new boolean[n+1][total/2+1];
 
         for(int i = 0;i<=n;i++){
             dp[i][0] = true;
         }
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=total;j++){
+            for(int j=1;j<=total/2;j++){
                 if(arr[i-1]<=j){
                     dp[i][j] = dp[i-1][j] || dp[i-1][j-arr[i-1]];
                 }
