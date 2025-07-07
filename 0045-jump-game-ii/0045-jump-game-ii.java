98586626932a59b293d7af6dc4c4c304370d602b
Muskan Darji <118981887+Muskan2403@users.noreply.github.com>
2025-07-07 11:05:03 +0530

    Time: 1 ms (99.37%), Space: 45.3 MB (50.52%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,37 +1,13 @@
 class Solution {
-    public int jump(int[] arr) {
-        int n = arr.length;
-        int[] dp = new int[n];
-        return helper(arr, n, dp, 0);
-    }
-
-    // private static int helper(int[] arr,int idx){
-    //     int n = arr.length;
-    //     if(idx>=n || idx == n-1) return 0;
-
-    //     for(int i = 1;i<=arr[idx];i++){
-    //         n = Math.min(n, 1+helper(arr,idx+i));
-    //     }
-    //     return n;
-    // }
-    private static int helper(int[] arr, int n, int[] dp, int i){
-        if(i==n-1) return 0;
-        if(i >=n) return Integer.MAX_VALUE;
-        if(dp[i] != 0){
-            return dp[i];
-        }
-        int mini = Integer.MAX_VALUE;
-        int jump = arr[i];
-
-        for(int j = 1;j<=jump;j++){
-            int step = i+j;
-            int ans = helper(arr,n,dp,step);
-        
-            if(ans!= Integer.MAX_VALUE){
-                mini = Math.min(mini, 1+ ans);
+    public int jump(int[] nums) {
+        int jump=0,fatherest=0,end=0;
+        for(int i=0;i<nums.length-1;i++){
+            fatherest=Math.max(fatherest,i+nums[i]);
+            if(i==end){
+                jump++;
+                end=fatherest;
             }
         }
-        return dp[i] = mini;
-
+        return jump;
     }
 }
