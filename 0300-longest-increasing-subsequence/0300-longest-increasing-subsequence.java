0102cf139be567bfa766123f935b35dba5c0edc0
Muskan Darji <118981887+Muskan2403@users.noreply.github.com>
2025-07-10 11:36:58 +0530

    Time: 37 ms (70.02%), Space: 44.2 MB (65.89%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -3,7 +3,6 @@ class Solution {
         int n = nums.length;
         int[] dp = new int[n];
         Arrays.fill(dp,1);
-        int len = 1;
 
         for(int i =1;i<n;i++){
             for(int j = 0;j<i;j++){
@@ -11,9 +10,11 @@ class Solution {
                     dp[i] = Math.max(dp[i], 1+dp[j]);
                 }
             }
-            len = Math.max(len,dp[i]);
         }
-        return len;
-        
+        int max = 0;
+        for(int len: dp){
+            max = Math.max(max,len);
+        }
+        return max;   
     }
 }
